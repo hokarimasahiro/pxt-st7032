f07615ca2476db4ae797a13f9a0636df51f7be3a
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-09 14:11:06 +0900

    更新 st7032.ts

--- st7032.ts
+++ st7032.ts
@@ -6,7 +6,7 @@ namespace st7032 {
     let i2cAddress = 0x3f;
     let topAddress = [0x00,0x40,0x14,0x54];
 
-    //% blockId="putCommand"
+    //% blockId="putCommand" block="put command"
     export function putCommand(data: number) {
         let buf = pins.createBuffer(2);
         buf[0] = 0x00;
@@ -14,7 +14,7 @@ namespace st7032 {
         pins.i2cWriteBuffer(i2cAddress, buf);
     }
 
-    //% blockId="putData"
+    //% blockId="putData" block="put data"
     export function putData(data: number) {
         let buf = pins.createBuffer(2);
         buf[0] = 0x40;
@@ -22,14 +22,14 @@ namespace st7032 {
         pins.i2cWriteBuffer(i2cAddress, buf);
     }
 
-    //% blockId="setAddress"
+    //% blockId="setAddress" block="set address"
     export function setAddress(ad:number): void {
         i2cAddress = ad;
     }
     /**
      * initLcd
      */
-    //% blockId="initLcd"
+    //% blockId="initLcd" block="init lcd"
     export function initLcd(): void {
         putCommand(0x38);
         putCommand(0x01);
@@ -37,13 +37,13 @@ namespace st7032 {
         putCommand(0x0c);
     }
 
-    //% blockId="setPosition"
+    //% blockId="setPosition" block="set position"
     export function setPosition(x:number,y:number): void {
         if((x > 3) || (y > 20)) return;
 
         putCommand( 0x80 + topAddress[x] + y);
     }
-    //% blockId="writeData"
+    //% blockId="writeData" block="write data"
     export function writeData(dt:string): void {
         for(let i = 0;i < dt.length;i++)
             putData(dt.charCodeAt(i));
