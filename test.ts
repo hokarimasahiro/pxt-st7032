4b6946a786d5aca6bb35d9cd1411ec31bf873444
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-09 15:06:20 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,6 @@
+basic.forever(function() {
     st7032.initLcd();
+    basic.pause(500)
     st7032.setPosition(0, 0);
     st7032.writeData("lcd write block");
     basic.pause(200)
@@ -6,7 +8,9 @@
     st7032.writeData("for micro:bit");
     basic.pause(200)
     st7032.setPosition(2, 0);
-    st7032.writeData("");
+    st7032.writeData("made by pokarihonpo");
     basic.pause(200)
     st7032.setPosition(3, 0);
-    st7032.writeData("by masahiro hokari");
+    st7032.writeData("  masahiro hokari");
+    basic.pause(2000);
+})
