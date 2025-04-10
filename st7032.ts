85e1e3b94b362efefd8c9f978626a74ee507a1fb
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-10 11:26:51 +0900

    更新 st7032.ts

--- st7032.ts
+++ st7032.ts
@@ -5,6 +5,7 @@
 namespace st7032 {
     let i2cAddress = 0x3f;
     let topAddress = [0x00,0x40,0x14,0x54];
+    let initFlag = 0;
 
     //% block="put command $data"
     //% block.loc.ja="コマンド|$data|の書き込み"
@@ -39,18 +40,20 @@ namespace st7032 {
         putCommand(0x01);
         putCommand(0x02);
         putCommand(0x0c);
+        initFlag = 1;
     }
 
     //% block="set position x $x y $y"
     //% block.loc.ja="表示開始位置を|$x|行、|$y|列にする"
     export function setPosition(x:number,y:number): void {
         if((x > 3) || (y > 20)) return;
-
+        if(initFlag = 0) initLcd();
         putCommand( 0x80 + topAddress[x] + y);
     }
     //% block="write data $dt"
     //% block.loc.ja="lcdに|$dt|を表示する"
     export function writeData(dt:string): void {
+        if (initFlag = 0) initLcd();
         for(let i = 0;i < dt.length;i++)
             putData(dt.charCodeAt(i));
     }
